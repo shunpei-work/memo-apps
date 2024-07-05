2d1afd2c29039fa49b93af33a7134a5b17ae22bf
shunpei <shunpei@localhost>
2024-07-05 22:43:49 +0900

    削除機能、編集機能追加

--- src/main/java/com/techacademy/controller/TopController.java
+++ src/main/java/com/techacademy/controller/TopController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.Memo;
 import com.techacademy.service.MemoService;
@@ -19,21 +24,80 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class TopController {
+    
+    // 固定メッセージ定義
+    public static final String deleteMsg = "削除が完了しました！";
+    public static final String editMsg = "更新が完了しました！";
+    
+    // 削除フラグ
+    public boolean deleteFlag = false;
+    // 編集フラグ
+    public boolean editFlag = false;
 
     @Autowired
     private MemoService memoService;
-
+    
     /**
-     * 値を全件取得して表示させる
+     * メモリストを取得
      */
-    @RequestMapping("top")
-    public String showMemoList(Model model){
+    @RequestMapping("/top")
+    public String getMemoList(Model model){
+        // 値を全件取得して表示させる
         List<Memo> memoInfoList = memoService.findAll(); 
-        model.addAttribute("memoInfoList", memoInfoList);
+        model.addAttribute("memoInfoList",memoInfoList);
+        // 削除していればメッセージ表示
+        if(deleteFlag == true) {
+            model.addAttribute("deleteMsg",deleteMsg);
+            deleteFlag = false;
+        }
         return "top";
+    } 
+    
+    /**
+     * 削除機能
+     */
+    @RequestMapping("top/delete")
+    public String delete(@RequestParam("id")long id,Model model){
+        try {         
+            memoService.delete(id);
+            deleteFlag = true;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/top";
     }
     
+    /**
+     * 更新ページアクセス
+     */
+    @GetMapping("top/edit/{id}")
+    public String edit(@PathVariable("id")long id,Model model,@ModelAttribute Memo memo) {
+        Memo memoForm = memoService.findById(id);
+        model.addAttribute("memoForm",memoForm);
+        return "edit";
+    }
     
-
+    /**
+     * 更新機能
+     */
+    @PostMapping("top/edit/{id}")
+    public String update(Memo memo,Model model) {
+        try {
+            memoService.save(memo);
+            editFlag = true;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        // 値を全件取得して表示させる
+        List<Memo> memoInfoList = memoService.findAll();
+        model.addAttribute("memoInfoList",memoInfoList);
+        // 編集していればメッセージ表示
+        if(editFlag == true) {
+            model.addAttribute("editMsg",editMsg);
+            editFlag = false;
+        }
+        return "top";
+    }
 }
--- src/main/java/com/techacademy/service/MemoService.java
+++ src/main/java/com/techacademy/service/MemoService.java
@@ -15,13 +15,40 @@ public class MemoService {
     @Autowired
     private MemoRepository memoRepository;
     
+    /**
+     * 全件取得機能
+     * @return List<Memo>
+     */
     @Transactional
     public List<Memo> findAll() {
         return memoRepository.findAll();
     }
     
+    // 登録機能
     @Transactional
-    public void save(Memo memo) {       
+    public void save(Memo memo) {
         memoRepository.saveAndFlush(memo);
+    }
+    
+    // 指定されたIDの列削除機能
+    @Transactional
+    public void delete(long id) {
+        memoRepository.deleteById(id);
+    }
+    
+    // 更新機能
+    @Transactional
+    public void update(Memo memo) {
+        memoRepository.save(memo);
+        
+    }
+    
+    /**
+     * IDに紐づくメモ情報を取得
+     * @param id メモID
+     * @return memo メモオブジェクト
+     */
+    public Memo findById(long id) {
+        return memoRepository.findById(id).get();
     }
 }
